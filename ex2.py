1d8813d055c0857fe9d00a98ceb8d44776beaa85
Andrei Martini <andreimartini1605@gmail.com>
2025-04-23 13:11:33 -0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,3 +1,4 @@
+#2. O pais A tem 1200000 habitantes e a taxa de crescimento da população é de 3% ao ano. O pais B tem 1500000 habitantes e a taxa de crescimento da população é de 2% ao ano. Faça um programa que calcule quantos anos são necessários para que o país A tenha mais população que o país B.
 pA = 1200000
 pB = 1500000
 cont = 0
