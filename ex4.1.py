1c043cf246ec11018298a63471a8d1c70e1e35b1
Andrei Martini <andreimartini1605@gmail.com>
2025-04-23 13:13:27 -0300

    Update ex4.1.py

--- ex4.1.py
+++ ex4.1.py
@@ -1,3 +1,4 @@
+# 4. Faça um programa que calcule o valor final de uma dívida de R$ 1.000,00, com uma taxa de juros de 1% ao mês, após 50 meses. Faça um programa usando o while e o mesmo programa usando for.
 div = 1000
 jur = 0.01
 cont = 0
