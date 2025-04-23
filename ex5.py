f098947fb199b605954608e67f693bd879ff61c0
Andrei Martini <andreimartini1605@gmail.com>
2025-04-23 13:14:02 -0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,3 +1,4 @@
+# 5. Faça um programa que calcule e escreva quantos meses são necessários para uma aplicação de R$ 1.000,00, com juros de 1% ao mês atingir R$ 2.000,00.
 div = 1000
 jur = 0.01
 cont = 0
