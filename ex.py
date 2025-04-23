e5efbb43d548d5bf174eb998c5d39887d3438c83
Andrei Martini <andreimartini1605@gmail.com>
2025-04-23 13:09:44 -0300

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,3 +1,5 @@
+#1. Faça um programa que leia um número não conhecido de valores correspondendo à quantidade de produtos e valor unitário do mesmo, contando a quantidade e somando o valor parcial da compra, até que seja informado a quantidade 0 (zero). Ao final, escreva a quantidade de itens comprados e o valor total da compra.
+
 valorT = 0
 cont = 0
 while True :
